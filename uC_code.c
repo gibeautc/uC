45cb7fc6c9a83f79243277f64a3a28ea37433ab8
Chad Gibeaut <gibeautc@oregonstate.edu>
2015-11-16 15:06:46 -0800

    added spi_init()

--- uC_code.c
+++ uC_code.c
@@ -1,16 +1,25 @@
 #define F_CPU  8000000UL
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
+
+//SPI init will set up SPI 
+void spi_init()
+{
+//DDRB need to be set correctly
+//Set at least 4 Chip select pins  3 sensors plus SRAM
+SPCR=(1<<SPE)|(1<<MSTR);  //master mode
+SPSR=(1<<SPI2X);          //sets 2x speed
+}//end spi_init()
 
 
 int main()
 {
- DDRB = 0xFF; 
+ 
+
+spi_init();
+
 while(1)
-  { 
-PORTB=(0<<1);
- _delay_ms(500);
-PORTB=(1<<1);
-  _delay_ms(500);  
+{ 
  } //end while 
 } //end main
